6e57e8b39233556b22b04397a4d1ec7e1e4af967
dinhquy-nguyen-1704 <nguyendinhquythptcla@gmail.com>
2024-03-11 10:23:28 +0700

    delete comments

--- chat_with_pdf/qa_bot.py
+++ chat_with_pdf/qa_bot.py
@@ -14,35 +14,6 @@ model_emb = GPT4AllEmbeddings(model_file=config.model_emb)
 question = config.question
 
 
-# def load_llm(model_file):
-#     llm = CTransformers(
-#         model=model_file,
-#         model_type='llama',
-#         max_new_tokens=1024,
-#         temperature=0.01
-#     )
-
-#     return llm
-
-# def create_prompt(template):
-#     prompt = PromptTemplate(template=template, input_variables=['context', 'question'])
-
-#     return prompt
-
-# def create_qa_chain(prompt, llm, db):
-#     llm_chain = RetrievalQA.from_chain_type(
-#         llm=llm,
-#         chain_type='stuff',
-#         retriever=db.as_retriever(search_kwargs={'k':3}, max_tokens_limit=1024),
-#         return_source_documents=False,
-#         chain_type_kwargs={'prompt':prompt}
-#     )
-#     return llm_chain
-
-# def read_vector_db(vector_db_path, model_emb):
-#     db = FAISS.load_local(vector_db_path, model_emb)
-#     return db
-
 db = read_vector_db(vector_db_path, model_emb)
 llm = load_llm(model_file)
 
